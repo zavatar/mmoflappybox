519461f5aa71bbfbe102a15502db9d09a8e08ba4
zhumeng <zhumeng1989@gmail.com>
2014-03-11 20:58:51 +0800

    Part II

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Massively multiplayer online flappy box Based on this [Blog]
 
+* Use [http-server] to serve *http://localhost:8080*
+
 [Blog]:http://blog.lessmilk.com/how-to-make-flappy-bird-in-html5-1/
+[http-server]:https://github.com/nodeapps/http-server
--- main.js
+++ main.js
@@ -1,87 +1,93 @@
-// Initialize Phaser, and creates a 400x490px game
 var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
 
-// Creates a new 'main' state that will contain the game
 var main_state = {
 
-    // Function called first to load all the assets
     preload: function() { 
-        // Change the background color of the game
         this.game.stage.backgroundColor = '#71c5cf';
-
-        // Load the bird sprite
         this.game.load.image('bird', 'assets/bird.png');  
-
-        // Load the pipe sprite
         this.game.load.image('pipe', 'assets/pipe.png');      
+
+        // Load jump sound
+        this.game.load.audio('jump', 'assets/jump.wav');
     },
 
-    // Fuction called after 'preload' to setup the game 
     create: function() { 
-        // Display the bird on the screen
-        this.bird = this.game.add.sprite(100, 245, 'bird');
-        
-        // Add gravity to the bird to make it fall
-        this.bird.body.gravity.y = 1000; 
-
-        // Call the 'jump' function when the spacekey is hit
         var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space_key.onDown.add(this.jump, this); 
 
-        // Create a group of 20 pipes
         this.pipes = game.add.group();
         this.pipes.createMultiple(20, 'pipe');  
-
-        // Timer that calls 'add_row_of_pipes' ever 1.5 seconds
         this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);           
 
-        // Add a score label on the top left of the screen
+        this.bird = this.game.add.sprite(100, 245, 'bird');
+        this.bird.body.gravity.y = 1000; 
+         // Change the anchor point of the bird
+        this.bird.anchor.setTo(-0.2, 0.5);
+               
         this.score = 0;
         var style = { font: "30px Arial", fill: "#ffffff" };
-        this.label_score = this.game.add.text(20, 20, "0", style);  
+        this.label_score = this.game.add.text(20, 20, "0", style); 
+
+        // Add sounds to the game
+        this.jump_sound = this.game.add.audio('jump');
+        this.hit_sound = this.game.add.audio('hit'); 
     },
 
-    // This function is called 60 times per second
     update: function() {
-        // If the bird is out of the world (too high or too low), call the 'restart_game' function
         if (this.bird.inWorld == false)
             this.restart_game(); 
 
-        // If the bird overlap any pipes, call 'restart_game'
-        this.game.physics.overlap(this.bird, this.pipes, this.restart_game, null, this);      
+        // Make the bird slowly rotate downward
+        if (this.bird.angle < 20)
+            this.bird.angle += 1;
+
+        this.game.physics.overlap(this.bird, this.pipes, this.hit_pipe, null, this);      
     },
 
-    // Make the bird jump 
     jump: function() {
-        // Add a vertical velocity to the bird
+        // if the bird hit a pipe, no jump
+        if (this.bird.alive == false)
+            return; 
+
         this.bird.body.velocity.y = -350;
+
+        // Animation to rotate the bird
+        this.game.add.tween(this.bird).to({angle: -20}, 100).start();
+
+        // Play a jump sound
+        this.jump_sound.play();
     },
 
-    // Restart the game
-    restart_game: function() {
-        // Remove the timer
+    // Dead animation when the bird hit a pipe
+    hit_pipe: function() {
+        // If the bird has already hit a pipe, we have nothing to do
+        if (this.bird.alive == false)
+            return;
+
+        // Set the alive flag to false
+        this.bird.alive = false;
+
+        // Prevent new pipes from apearing
         this.game.time.events.remove(this.timer);
 
-        // Start the 'main' state, which restarts the game
+        // Go trough all the pipes, and stop their movement
+        this.pipes.forEachAlive(function(p){
+            p.body.velocity.x = 0;
+        }, this);
+    },
+
+    restart_game: function() {
+        this.game.time.events.remove(this.timer);
         this.game.state.start('main');
     },
 
-    // Add a pipe on the screen
     add_one_pipe: function(x, y) {
-        // Get the first dead pipe of our group
         var pipe = this.pipes.getFirstDead();
-
-        // Set the new position of the pipe
         pipe.reset(x, y);
-
-         // Add velocity to the pipe to make it move left
         pipe.body.velocity.x = -200; 
-               
-        // Kill the pipe when it's no longer visible 
         pipe.outOfBoundsKill = true;
     },
 
-    // Add a row of 6 pipes with a hole somewhere in the middle
     add_row_of_pipes: function() {
         var hole = Math.floor(Math.random()*5)+1;
         
@@ -94,6 +100,5 @@ var main_state = {
     },
 };
 
-// Add and start the 'main' state to start the game
 game.state.add('main', main_state);  
 game.state.start('main'); 
